15ea877aedd15a6e9d086b5330449397a70a59ea
ChloeBallantyne <160100291+ChloeBallantyne@users.noreply.github.com>
2024-04-22 14:44:26 +1000

    added styling to page

--- src/pages/SetPage/SetPage.jsx
+++ src/pages/SetPage/SetPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./SetPage.module.scss";
 
 const SetPage = ({ set }) => {
   // const navigate = useNavigate();
@@ -10,10 +11,14 @@ const SetPage = ({ set }) => {
       <h3>Price: ${set.price}</h3>
       <h5>SKU: {set.sku}</h5>
       <h5>Quantity: {set.quantity}</h5>
-      <img src={set.imageLink} alt={set.name} />
+      <div className={styles.wrapper1}>
+        <img src={set.imageLink} alt={set.name} />
+        <div className={styles.wrapper2}>
+          <h5>Description:</h5>
+          <p>{set.description}</p>
+        </div>
+      </div>
       <h5>Release: {set.release}</h5>
-      <h5>Description:</h5>
-      <p>{set.description}</p>
     </main>
   );
 };
